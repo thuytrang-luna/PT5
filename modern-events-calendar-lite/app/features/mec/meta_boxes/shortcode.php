da1972f9b13f7123497d357000e34fdd816edd4d
Chann Channn <lethithuytrang.isu@gmail.com>
2022-03-24 09:48:55 +0700

    first commit

---
+++ modern-events-calendar-lite/app/features/mec/meta_boxes/shortcode.php
@@ -0,0 +1,6 @@
+<?php
+/** no direct access **/
+defined('MECEXEC') or die();
+?>
+<div class="mec-calendar-metabox mec-shortcode" id="MECCopyCode" onclick="mec_copy_code()" title="<?php __('Click to copy shortcode', 'modern-events-calendar-lite'); ?>">[MEC id="<?php echo $post->ID; ?>"]</div>
+<span class="mec-copied">Copied!</span>
